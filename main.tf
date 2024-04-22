6ce902f177b33896d53478dda4e54f3828596444
SINGH SNEH <25021804@mahindra.com>
2024-04-22 18:06:28 +0530

    inital setup of alb for umig

---
+++ main.tf
@@ -0,0 +1,101 @@
+resource "google_project_service" "certapi" {
+  project = var.project_id
+  service = "certificatemanager.googleapis.com"
+}
+
+resource "google_compute_instance_group" "instance_group" {
+  project     = var.project_id
+  name        = "${var.name}-umig"
+  description = var.description
+  zone        = var.zone
+  instances   = var.instances
+  network     = var.network
+  dynamic "named_port" {
+    for_each = var.enable_named_port ? [{}] : []
+    content {
+      name = "https"
+      port = var.port
+    }
+  }
+  named_port {
+          name = "https" 
+          port = var.port
+        }
+   
+}
+
+resource "google_compute_global_address" "default" {
+  project      = var.project_id
+  name       = "${var.name}-ip"
+  ip_version = "IPV4"
+  address_type = "EXTERNAL"
+}
+resource "google_compute_health_check" "default" {
+    project         = var.project_id
+  name               = "${var.name}-health-check"
+  check_interval_sec = 5
+  healthy_threshold  = 2
+  tcp_health_check {
+    port               = var.port
+    port_specification = "USE_FIXED_PORT"
+    proxy_header       = "PROXY_V1"
+    
+  }
+  timeout_sec         = 5
+  unhealthy_threshold = 2
+}
+
+resource "google_compute_backend_service" "default" {
+  project               = var.project_id
+  name                            = "${var.name}-back-end"
+  connection_draining_timeout_sec = 0
+  health_checks                   = [google_compute_health_check.default.id]
+  load_balancing_scheme           = "EXTERNAL"
+  port_name                       = "https"
+  protocol                        = "HTTPS"
+  session_affinity                = "NONE"
+  timeout_sec                     = 30
+  backend {
+    group           = google_compute_instance_group.instance_group.id
+    balancing_mode  = "UTILIZATION"
+    capacity_scaler = 1.0
+    max_utilization  = 0.8
+    
+  }
+}
+
+resource "google_compute_global_forwarding_rule" "forwarding_rule" {
+  project               = var.project_id
+  name                  = "${var.name}-forwarding-rule"
+  target                = google_compute_target_https_proxy.target-proxy.id
+  ip_protocol           = "TCP"
+  load_balancing_scheme = "EXTERNAL"
+  port_range            = "443"
+  ip_address            = google_compute_global_address.default.address
+   depends_on = [
+    google_compute_target_https_proxy.target-proxy
+  ]
+}
+
+resource "google_compute_target_https_proxy" "target-proxy" {
+  project = var.project_id
+  ssl_certificates = var.ssl_certificates
+  name    = "${var.name}-target-proxy"
+  url_map = google_compute_url_map.url_map.id
+  depends_on = [
+    # google_compute_ssl_certificate.non-prod,
+    # google_compute_ssl_certificate.prod,
+    google_compute_url_map.url_map
+  ]
+  
+}
+
+resource "google_compute_url_map" "url_map" {
+  project         = var.project_id
+  name            = "${var.name}-url-map"
+  default_service = google_compute_backend_service.default.id
+  depends_on = [
+    google_compute_backend_service.default
+  ]
+}
+
---
+++ variables.tf
@@ -0,0 +1,52 @@
+variable "name" {
+  type        = string
+  description = "The name of the instance group"
+  default = "test-alb-terraform"
+}
+variable "description" {
+  type        = string
+  description = "description of the instance group"
+  default = "Application load balancer for unmanaged instance"
+}
+variable "zone" {
+  type        = string
+  description = "The zone that this instance group should be created in."
+  default = "asia-south1-a"
+}
+variable "project_id" {
+  type        = string
+  description = " The ID of the project in which the resource belongs. If it is not provided, the provider project is used."
+  default = "mpaas-sandbox-965286"
+}
+variable "network" {
+  type        = string
+  description = "The URL of the network the instance group is in. If this is different from the network where the instances are in, the creation fails."
+  default = "projects/host-project-dev-env-mum/global/networks/shared-vpc-development-01"
+}
+variable "port" {
+  type        = string
+  description = "The name which the port will be mapped to."
+  default     = "443"
+
+}
+variable "named_port_name_port" {
+  type        = string
+  description = "The port number to map the name to."
+  default     = "https"
+}
+variable "instances" {
+  type        = list(string)
+  description = "The list of instances in the group, in self_link format. When adding instances they must all be in the same network and zone as the instance group."
+  default = [ "projects/mpaas-sandbox-965286/zones/asia-south1-a/instances/vm01" ]
+}
+variable "enable_named_port" {
+  type        = bool
+  description = "enable the port"
+  default     = false
+}
+variable "ssl_certificates" {
+    type=list(string)
+    description = "SSL certificate"
+    default = ["projects/mpaas-sandbox-965286/global/sslCertificates/m-devsecops-com"]
+  
+}
